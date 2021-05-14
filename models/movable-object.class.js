67e728615bbff6a1169586bf1f75c65a6dd8af98
Crunck78 <crunck78@googlemail.com>
2021-05-14 14:28:59 +0200

    08 - Bilder einfügen

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,6 +3,12 @@ class MovableObject{
     y = 400;
     img;
 
+    //loadImage('img/test.png')
+    loadImage(path){
+        this.img = new Image(); //this.img = document.getElementById('image) <img id="image" src>
+        this.img.src = path;
+    }
+
     moveRight(){
         console.log('Moving right');
     }

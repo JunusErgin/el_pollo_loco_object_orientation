ad6f0120fc5aba2d8691766bd9e8fcc587763087
Crunck78 <crunck78@googlemail.com>
2021-05-14 14:14:14 +0200

    07 - Klassen anlegen

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+let world = new World();
 
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    console.log('My Character is', character);
+    console.log('My Character is', world.character);
 }

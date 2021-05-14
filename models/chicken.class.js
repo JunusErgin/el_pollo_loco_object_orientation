0f1d8922da3dc072731015f2316a95b957f899ba
Crunck78 <crunck78@googlemail.com>
2021-05-14 16:56:35 +0200

    11 - Hühnchen platzieren 2:30 bis Ende

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,6 +2,6 @@ class Chicken extends MovableObject{
     constructor(){
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
         
-        this.x = 200;
+        this.x = 200 + Math.random() * 500; //Zahl zwieschen 200 und 700
     }
 }

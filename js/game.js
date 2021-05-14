4d86fc77deaa335c520a5f77450a1b6ed9f724b9
Crunck78 <crunck78@googlemail.com>
2021-05-14 13:17:36 +0200

    05 - Erste Klasse implementieren

--- js/game.js
+++ js/game.js
@@ -1,12 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    character.src ='../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);
+    console.log('My Character is', character);
 }
---
+++ models/movable-object.class.js
@@ -0,0 +1,13 @@
+class MovableObject{
+    x = 120;
+    y = 400;
+    img;
+
+    moveRight(){
+        console.log('Moving right');
+    }
+    
+    moveLeft(){
+        console.log('Moving left');
+    }
+}

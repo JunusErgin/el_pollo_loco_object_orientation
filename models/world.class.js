bb3b143cb534edead127fdfd7d93364becc37db7
Crunck78 <crunck78@googlemail.com>
2021-05-14 14:52:00 +0200

    10 - requestAnimationFrame()

--- models/world.class.js
+++ models/world.class.js
@@ -6,13 +6,26 @@ class World{
         new Chicken()
     ];
     ctx;
+    canvas;
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
+    //Draw() wird immer wieder aufgerufen
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach( enemy =>{
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }

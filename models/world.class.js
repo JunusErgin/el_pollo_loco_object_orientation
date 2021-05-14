654e8a10ddcde85eecad866b32a5c4d6e2e0b820
Crunck78 <crunck78@googlemail.com>
2021-05-14 14:38:11 +0200

    09 - Character anzeigen

--- models/world.class.js
+++ models/world.class.js
@@ -5,8 +5,14 @@ class World{
         new Chicken(),
         new Chicken()
     ];
+    ctx;
+
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw(){
-        
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
     }
 }

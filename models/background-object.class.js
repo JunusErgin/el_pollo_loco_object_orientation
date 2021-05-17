5e050adabca07c8b3d5d0dadd5d2379c5dde90d4
Crunck78 <crunck78@googlemail.com>
2021-05-17 13:40:23 +0200

    13 - Hintergrundobjekte und Architektur 9:45 bis Ende

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,5 +1,10 @@
 class BackgroundObject extends MovableObject{
-    constructor(imagePath){
+
+    width = 720;
+    height = 400;
+    constructor(imagePath, x, y){
         super().loadImage(imagePath);
+        this.x = x;
+        this.y = y;
     }
 }

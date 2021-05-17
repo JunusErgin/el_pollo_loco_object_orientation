4503e645f73e833d7c0520296b741024e1a8f865
Crunck78 <crunck78@googlemail.com>
2021-05-17 15:44:24 +0200

    06 - Keyboard Objekt

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init(){
     canvas = document.getElementById('canvas');
@@ -7,3 +8,7 @@ function init(){
 
     console.log('My Character is', world.character);
 }
+
+window.addEventListener('keypress', (e)=>{
+    console.log(e);
+});

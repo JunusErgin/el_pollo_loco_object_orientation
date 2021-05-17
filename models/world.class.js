6219f3e2df872e73a99e1fe26cd6781e39d2cb1e
Crunck78 <crunck78@googlemail.com>
2021-05-17 14:28:01 +0200

    14 - Weitere Hintergrundobjekte 1:30 bis 4:45

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,9 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0)
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)
     ];
     ctx;
     canvas;
@@ -24,10 +26,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.backgroundObjects);
 
         let self = this;
         requestAnimationFrame(function () {

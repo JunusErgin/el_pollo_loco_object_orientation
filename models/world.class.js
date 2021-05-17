aa456f6b0ca6ac065197019d29a4851919f888d2
Crunck78 <crunck78@googlemail.com>
2021-05-17 16:51:28 +0200

    13 - Level Objekt

--- models/world.class.js
+++ models/world.class.js
@@ -1,40 +1,8 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719 * 2),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3),
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     ctx;
     canvas;
     keyboard;

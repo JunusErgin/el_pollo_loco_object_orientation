1b258a5617bbdcd5fea9dd9d858e507554876c00
Crunck78 <crunck78@googlemail.com>
2021-05-17 14:43:02 +0200

    15 - Kleine Optimierunge 4:00 bis Ende

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,7 @@
 class Chicken extends MovableObject{
+    height = 55;
+    width = 70;
+    y = 360;
     constructor(){
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
         

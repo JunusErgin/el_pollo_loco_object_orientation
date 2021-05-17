580d65bef42b788ca0d38c62c2c9c297b39cdf55
Crunck78 <crunck78@googlemail.com>
2021-05-17 15:11:25 +0200

    02 - ImageCache

--- models/character.class.js
+++ models/character.class.js
@@ -4,6 +4,14 @@ class Character extends MovableObject{
     y = 180;
     constructor(){
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
+        this.loadImages([
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
+            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+        ]);
     }
 
     jump(){
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,21 +1,34 @@
-class MovableObject{
+class MovableObject {
     x = 120;
     y = 280;
     height = 150;
     width = 100;
     img;
+    imageCache = {};
 
     //loadImage('img/test.png')
-    loadImage(path){
+    loadImage(path) {
         this.img = new Image(); //this.img = document.getElementById('image) <img id="image" src>
         this.img.src = path;
     }
 
-    moveRight(){
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+    }
+
+    moveRight() {
         console.log('Moving right');
     }
-    
-    moveLeft(){
+
+    moveLeft() {
         console.log('Moving left');
     }
 }

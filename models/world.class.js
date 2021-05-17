42fb21fce866419427faa672292acccdd21e8858
Crunck78 <crunck78@googlemail.com>
2021-05-17 14:34:53 +0200

    15 - Kleine Optimierungen Beginn bis 1:40

--- models/world.class.js
+++ models/world.class.js
@@ -9,6 +9,7 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)

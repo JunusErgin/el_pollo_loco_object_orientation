297f4dfdd12a89fe0d887923a6d5cbf4fd5fd43b
Crunck78 <crunck78@googlemail.com>
2021-05-17 14:39:58 +0200

    15 - Kleine Optimierungen 1:40 bis 4:00

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,7 @@
 class BackgroundObject extends MovableObject{
 
     width = 720;
-    height = 400;
+    height = 480;
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
         this.x = x;
--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject{
 
+    height = 250;
+    y = 180;
     constructor(){
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
     }

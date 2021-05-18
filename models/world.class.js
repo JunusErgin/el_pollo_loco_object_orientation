86394674e556164edc4e67854a600d58b0d3ca52
Crunck78 <crunck78@googlemail.com>
2021-05-18 16:03:08 +0200

    19 - Statusbar IIIII

--- models/world.class.js
+++ models/world.class.js
@@ -28,6 +28,7 @@ class World {
             this.level.enemies.forEach( (enemy)=>{
                 if(this.character.isColliding(enemy)){
                     this.character.hit();
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             });
         },  200);
@@ -38,9 +39,14 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0); //Back
+        // --------- Space for fixed Objects ---------
         this.addToMap(this.statusBar);
+        // --------- Space for fixed Objects End ---------
+        this.ctx.translate(this.camera_x, 0); //Forwards
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);

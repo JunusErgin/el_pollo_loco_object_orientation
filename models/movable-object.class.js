21ec378b0c876b079c0aefbaebea38f488bbe609
Crunck78 <crunck78@googlemail.com>
2021-05-18 14:59:59 +0200

    16 - Statusbar II

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,5 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    height = 150;
-    width = 100;
-    img;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject{
+   
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,28 +18,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.y < 180;
-    }
-
-    //loadImage('img/test.png')
-    loadImage(path) {
-        this.img = new Image(); //this.img = document.getElementById('image) <img id="image" src>
-        this.img.src = path;
-    }
-
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {

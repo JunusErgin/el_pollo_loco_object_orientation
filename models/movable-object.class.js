76fd30e5f94459b0291268a83b9a5a78066ab6ad
Crunck78 <crunck78@googlemail.com>
2021-05-18 14:21:51 +0200

    09 - Instanceof

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -47,11 +47,13 @@ class MovableObject {
     }
 
     drawFrame(ctx){
-        ctx.beginPath();
-        ctx.lineWidth = '5';
-        ctx.strokeStyle = 'blue';
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
+        if(this instanceof Character || this instanceof Chicken){
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
 
     playAnimation(images) {
